a17c82532e77fb95111f1acf24e1040d30f3fed6
sd8917 <sudhanshuraj8917@gmail.com>
2019-11-01 11:32:07 +0530

    Third commit

--- e_learn/urls.py
+++ e_learn/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('homeapp.urls')),
-    path('blog/',include('blog.urls'))
+    path('blog/',include('blog.urls')),
+    path('contact/',include('contact.urls'))
 ]
